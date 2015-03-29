ae1bf7636b8a8117ded9fb24f43736ea4ff4371a
maximkfd <ipust13@gmail.com>
2015-03-29 12:35:44 +0300

    Initial commit

---
+++ db_config.php
@@ -0,0 +1,7 @@
+<?php
+ 
+define('DB_USER', "devcol_test"); // db user
+define('DB_PASSWORD', "zuif1b3e"); // db password 
+define('DB_DATABASE', "devcol_test"); // database name
+define('DB_SERVER', "localhost"); // db server
+?>
---
+++ index.php
@@ -0,0 +1,1 @@
+<h1>DevColibri.com</h1>
